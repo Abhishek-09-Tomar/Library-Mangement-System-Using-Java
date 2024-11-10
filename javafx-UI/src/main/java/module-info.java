a082e623fe82afe0ef79065254dc233eb46975ed
Abhishek Tomar <164189524+Abhishek-09-Tomar@users.noreply.github.com>
2024-11-10 08:50:32 +0530

    Delete javafx-UI directory

--- javafx-UI/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.javafxui {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.javafxui to javafx.fxml;
-    exports com.example.javafxui;
-}
